6489147830ba36ee7cacc37f87ca034845bc16dd
Basmala@ <basmalaibrahim2027@gmail.com>
2024-12-04 12:37:14 +0200

    Update checkout.js

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -1,14 +1,9 @@
-document.getElementById('checkout-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-         window.location.href = "../../index.html"; // Redirect to index.html
-
-    alert('Payment successful! Thank you for your purchase.');
-});
 
 
 // Handle checkout form submission
 document.getElementById("checkout-form")?.addEventListener("submit", function (event) {
     event.preventDefault();
     alert("Payment successful! Thank you for your purchase.");
+    window.location.href = "../../index.html"; // Redirect to index.html
     localStorage.removeItem("cart"); // Clear cart after successful checkout
 });
