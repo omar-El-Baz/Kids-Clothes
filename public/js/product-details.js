e624d0e1a0fdc4002e6f4978ea0e51509e0bf893
MALAKK77 <malakgehad2004@gmail.com>
2024-12-04 08:36:55 +0200

    Update product-details.js

--- public/js/product-details.js
+++ public/js/product-details.js
@@ -1,31 +1,36 @@
 let wishlist = [];
-let cart = [];
-const addToCartButton = document.getElementById('addToCart');
-const addToFavoriteButton = document.getElementById('addToFavorite');
-const product = {
-   title: document.querySelector('.product-title').textContent,
-   price: document.querySelector('.current-price').textContent,
-   imgSrc: document.querySelector('.product-image img').src,
-};
-function addToCart() {
-   if (!cart.some(item => item.title === product.title)) {
-       cart.push(product);
-       alert(`${product.title} has been added to your cart.`);
-   } else {
-       alert(`${product.title} is already in your cart.`);
-   }
-   console.log("Cart:", cart); 
-}
-function addToWishlist() {
-    let wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
-    if (!wishlist.some(item => item.title === product.title)) {
-        wishlist.push(product);
-        localStorage.setItem('wishlist', JSON.stringify(wishlist));
-        alert(`${product.title} has been added to your wishlist.`);
-    } else {
-        alert(`${product.title} is already in your wishlist.`);
-    }
-    console.log("Wishlist:", wishlist);
+const emptyMessage = document.getElementById('emptyMessage');
+const wishlistContainer = document.getElementById('wishlistContainer');
+const wishlistItemsContainer = document.querySelector('.wishlist-items');
+function addToWishlist(name, imgSrc, price) {
+ if (!wishlist.some(item => item.name === name)) {
+   wishlist.push({ name, imgSrc, price });
+   updateWishlist();
  }
-addToCartButton.addEventListener('click', addToCart);
-addToFavoriteButton.addEventListener('click', addToWishlist);
+}
+function removeFromWishlist(name) {
+ wishlist = wishlist.filter(item => item.name !== name);
+ updateWishlist();
+}
+function updateWishlist() {
+ if (wishlist.length === 0) {
+   emptyMessage.style.display = 'flex';
+   wishlistContainer.style.display = 'none';
+ } else {
+   emptyMessage.style.display = 'none';
+   wishlistContainer.style.display = 'block';
+   wishlistItemsContainer.innerHTML = wishlist
+     .map(
+       item => `
+<div class="wishlist-item">
+<img src="${item.imgSrc}" alt="${item.name}">
+<p>${item.name}</p>
+<p class="price">${item.price}</p>
+<button class="remove-button" onclick="removeFromWishlist('${item.name}')">Remove</button>
+</div>`
+     )
+     .join('');
+ }
+}
+ 
+updateWishlist();
